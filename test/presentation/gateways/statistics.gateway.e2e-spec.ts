eb3e99bef9e6aa17c7f1e55034e0fbfdbd5631a9
José Ribamar Pilar de Araújo <jose.21rb@outlook.com>
2025-05-12 16:58:24 -0300

    feat:  teste de integração para statistics.gateway

---
+++ test/presentation/gateways/statistics.gateway.e2e-spec.ts
@@ -0,0 +1,48 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { io as Client, Socket as ClientSocket } from 'socket.io-client';
+
+describe('StatisticsGateway (e2e)', () => {
+    let app: INestApplication;
+    let clientSocket: ClientSocket;
+  
+    beforeAll(async () => {
+      const moduleFixture = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+  
+      app = moduleFixture.createNestApplication();
+      await app.init();
+      await app.listen(3000); // essencial para socket funcionar
+  
+      clientSocket = Client(`http://localhost:3000/statistics`, {
+        transports: ['websocket'],
+      });
+  
+      await new Promise<void>((resolve) => {
+        clientSocket.on('connect', () => resolve());
+      });
+    }, 15000); // aumenta timeout
+  
+    afterAll(async () => {
+      if (clientSocket.connected) {
+        clientSocket.disconnect();
+      }
+      await app.close();
+    });
+  
+    it('deve receber estatísticas via evento statisticsUpdate ao enviar requestStatistics', (done) => {
+      clientSocket.once('statisticsUpdate', (data) => {
+        expect(data).toHaveProperty('count');
+        expect(data).toHaveProperty('sum');
+        expect(data).toHaveProperty('avg');
+        expect(data).toHaveProperty('min');
+        expect(data).toHaveProperty('max');
+        done();
+      });
+  
+      clientSocket.emit('requestStatistics');
+    });
+  });
+  
